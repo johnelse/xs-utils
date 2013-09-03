ae88988089f4cab0f7cd0595866dc6d5528f542d
John Else <john.else@citrix.com>
2013-09-03 16:20:51 +0100

    Rearrange code and add comments
    Signed-off-by: John Else <john.else@citrix.com>

--- check_host.sh
+++ check_host.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 . /etc/xensource-inventory
-domains=`list_domains -minimal`
 me=$INSTALLATION_UUID
 
 errorsdetected=0
 
+# Get names of domains running on this host.
+domains=`list_domains -minimal`
+
+# Check that each of the names given by list_domains corresponds to the UUID of
+# a VM marked as resident on this host in xapi.
 for i in $domains
 do
 	echo "checking domain $i"
@@ -17,8 +21,11 @@ do
 	fi
 done
 
+# Get VDIs which have a tapdisk running on this host.
 vdis=`tap-ctl list | rev | cut -d/ -f1 | rev | grep -o '[0-9a-f][0-9a-f-]\+'`
 
+# Check each of these corresponds to an attached VBD owned by
+# a locally-resident VM.
 for i in $vdis
 do
 	echo "Checking VDI $i"
