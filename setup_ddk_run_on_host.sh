5a0bd033f0a159fab88e7df3cac4fd10a8c165c7
John Else <john.else@citrix.com>
2013-09-19 14:41:23 +0100

    Set name-label of the new VM
    Signed-off-by: John Else <john.else@citrix.com>

--- setup_ddk_run_on_host.sh
+++ setup_ddk_run_on_host.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-    echo "usage: $0 <branch> <sr-uuid>"
+    echo "usage: $0 <branch> <sr-uuid> <name-label>"
     exit 1
 fi
 
 BRANCH=$1
 SR_UUID=$2
+NAME_LABEL=$3
 MOUNT_POINT=/mnt
 
 set -x
@@ -20,6 +21,6 @@ VM=`xe vm-import filename=${MOUNT_POINT}/ddk/ova.xml sr-uuid=${SR_UUID}`
 
 # Setup the VM.
 xe vm-memory-limits-set uuid=${VM} dynamic-min=2147483648 dynamic-max=2147483648 static-min=2147483648 static-max=2147483648
-xe vm-param-set uuid=${VM} VCPUs-max=4 VCPUs-at-startup=4
+xe vm-param-set uuid=${VM} VCPUs-max=4 VCPUs-at-startup=4 name-label=${NAME_LABEL}
 VDI=`xe vdi-create type=user sr-uuid=${SR_UUID} name-label="DDK data VDI" virtual-size=21474836480`
 xe vbd-create vm-uuid=${VM} vdi-uuid=${VDI} device=autodetect type=Disk mode=RW
